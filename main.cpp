dbd7e41e6db47a208f4f4546b10eca1cf8d9c4bc
AngryFender <teabiscuitperson@gmail.com>
2025-12-02 23:52:42 +0000

    Added some curlcpp example code to get it started

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 ﻿#include <iostream>
-
-using namespace std;
+#include "WinSock2.h"
+#include "curlcpp/curl_easy.h"
+using curl::curl_easy;
+using curl::curl_easy_exception;
+using curl::curlcpp_traceback;
 
 int main()
 {
-    std::cout << "Hello World" << endl;
+    std::cout << "Hello World" << std::endl;
+
+    // Easy object to handle the connection.
+    curl_easy easy;
+
+    // Add some options.
+    easy.add<CURLOPT_URL>("www.google.com");
+    easy.add<CURLOPT_FOLLOWLOCATION>(1L);
+
+    try {
+        easy.perform();
+    } catch (curl_easy_exception &error) {
+        // If you want to print the last error.
+        std::cerr<<error.what()<<std::endl;
+    }
     return 0;
 }
